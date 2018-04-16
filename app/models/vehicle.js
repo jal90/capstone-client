3e1586e0c04efbdfb65371946f0e86308694c4fc
jal90 <mezzyjezze@gmail.com>
2018-04-16 12:27:45 -0400

    Add mileage to vehicle model
    Included it in show vehicle view and update field

--- app/models/vehicle.js
+++ app/models/vehicle.js
@@ -3,5 +3,6 @@ import DS from 'ember-data'
 export default DS.Model.extend({
   make: DS.attr('string'),
   model: DS.attr('string'),
-  year: DS.attr('string')
+  year: DS.attr('string'),
+  mileage: DS.attr('string')
 })
--- app/routes/vehicle.js
+++ app/routes/vehicle.js
@@ -7,6 +7,9 @@ export default Route.extend({
 
   actions: {
     updateVehicle (vehicle) {
+      // TODO: add validation so that year and mileage are only numbers
+      // maybe allow mileage to accept commas, or instruct user to not
+      // use commas
       vehicle.save()
     },
     deleteVehicle (vehicle) {
