48f40f12f008eea2f4cce231cb453140908c397d
jal90 <mezzyjezze@gmail.com>
2018-04-16 17:02:51 -0400

    Exploring styling in update route
    Playing around with how to style ember elements using classNames on the component
    TODO: nicely align fields, perhaps with bootstrap columns, but make sure not to break
    the functionality of the form and button

--- app/components/update-vehicle.js
+++ app/components/update-vehicle.js
@@ -1,6 +1,8 @@
 import Component from '@ember/component'
 
 export default Component.extend({
+  classNames: ['inline'],
+
   actions: {
     updateVehicle (vehicle) {
       this.sendAction('updateVehicle', vehicle)
