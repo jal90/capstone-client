14ee98680b947f26286f832f5144ed5481be4fc6
jal90 <mezzyjezze@gmail.com>
2018-04-16 21:30:49 -0400

    Use didInsertElement to clear formfields on page load

--- app/components/new-vehicle.js
+++ app/components/new-vehicle.js
@@ -1,12 +1,25 @@
 import Component from '@ember/component'
 
 export default Component.extend({
-  vehicle: {
-    make: '',
-    model: '',
-    year: '',
-    mileage: ''
+  didInsertElement () {
+    this.set('vehicle', {
+      // These fields aren't necessary, I just included them to be explicit
+      // The keys and values are actually being set by the form in the hbs file
+      //   make: '',
+      //   model: '',
+      //   year: '',
+      //   mileage: ''
+    })
   },
+  // Code below is valid, but code above will reset fields each time page
+  // is loaded! And either version is required to send the data up the chain.
+  // When `createVehicle` gets called, these options are how it finds this.get('vehicle')
+  // vehicle: {
+  //   make: '',
+  //   model: '',
+  //   year: '',
+  //   mileage: ''
+  // },
 
   // classNameBindings: ['showCreateForm'],
   // showCreateForm: true,
