e3a3f128ff4495832a304e354065b8a2f0d9439b
jal90 <mezzyjezze@gmail.com>
2018-04-18 15:31:03 -0400

    Fix transitionTo from update-work to vehicle route
    So the last commit on branch 'styling' actually has the message for this
    commit. Used string interpolation to get the transition to working.
    
    The styling commit message should have read:
    put signup and signin into container-fluid divs and made them 'inline' style
    which limits the width of input fields

--- app/routes/update-work.js
+++ app/routes/update-work.js
@@ -1,21 +1,12 @@
 import Route from '@ember/routing/route'
 
 export default Route.extend({
-  // This model hook is being ignored, Ember is choosing the implicit one
-  model (params) {
-    return this.get('store').findRecord('work', params.work_id)
-  },
-
-  work: {
-    // vehicleId: work.vehicle_id
-  },
-
   actions: {
     updateWork (work) {
       console.log('work.data is ', work.data)
       work.save()
         .then(() => this.toast.info('Updated record!'))
-        .then(() => this.transitionTo(`/vehicles/${work.vehicle.id}`))
+        .then(() => this.transitionTo(`/vehicles/${work.get('vehicle.id')}`))
     }
   }
 })
