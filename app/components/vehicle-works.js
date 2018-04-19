becdaf818f5bb62ea4d6425270d78fb67331ec73
jal90 <mezzyjezze@gmail.com>
2018-04-19 13:42:23 -0400

    Install ember-moment and jspdf, update form fields
    Made work dates formatted into MM-DD-YYYY as opposed to YYYY-MM-DD
    
    Also added some require statments to forms - at first I required dates
    since ember-moment would render 'Invalid Date' if date was left blank,
    then I realized you could teach it to accept blank inputs.
    
    But in doing so I realized my mileage-format helper would wreck things if
    mileage was left blank, and unable to debug for now, I made mileage a required
    field for creating and updating works

--- app/components/vehicle-works.js
+++ app/components/vehicle-works.js
@@ -1,6 +1,7 @@
 import Component from '@ember/component'
 
 export default Component.extend({
+
   actions: {
     deleteWork (work) {
       this.sendAction('deleteWork', work)
--- app/helpers/format-mileage.js
+++ app/helpers/format-mileage.js
@@ -2,11 +2,13 @@ import { helper } from '@ember/component/helper'
 
 export function formatMileage (params/*, hash */) {
   const mileage = params[0]
-  console.log(params[0])
+
   const mileageWithCommas = (x) => {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
   }
-  return mileageWithCommas(mileage)
+  if (mileage !== undefined) {
+    return mileageWithCommas(mileage)
+  }
 }
 
 export default helper(formatMileage)
