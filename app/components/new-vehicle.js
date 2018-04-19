8bf112bed12c0635c1091bb82f0ad944d3f43705
jal90 <mezzyjezze@gmail.com>
2018-04-19 16:12:58 -0400

    Remove console log and commented code from new-vehicle component

--- app/components/new-vehicle.js
+++ app/components/new-vehicle.js
@@ -2,35 +2,12 @@ import Component from '@ember/component'
 
 export default Component.extend({
   didInsertElement () {
-    this.set('vehicle', {
-      // These fields aren't necessary, I just included them to be explicit
-      // The keys and values are actually being set by the form in the hbs file
-      //   make: '',
-      //   model: '',
-      //   year: '',
-      //   mileage: ''
-    })
+    this.set('vehicle', {})
   },
-  // Code below is valid, but code above will reset fields each time page
-  // is loaded! And either version is required to send the data up the chain.
-  // When `createVehicle` gets called, these options are how it finds this.get('vehicle')
-  // vehicle: {
-  //   make: '',
-  //   model: '',
-  //   year: '',
-  //   mileage: ''
-  // },
-
-  // classNameBindings: ['showCreateForm'],
-  // showCreateForm: true,
 
   actions: {
     createVehicle () {
       this.sendAction('createVehicle', this.get('vehicle'))
     }
-    // toggleCreateForm () {
-    //   console.log('button clicked')
-    //   this.toggleProperty('showCreateForm')
-    // }
   }
 })
